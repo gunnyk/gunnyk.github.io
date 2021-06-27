dc83cd1327d6ba82a5a4d9485605f3a3d34c73d3
김건 <79524742+GunnKimKR@users.noreply.github.com>
2021-06-27 16:09:16 +0900

    feat: add comments widget built on GitHub issues

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210627.1602';
+const cacheName = 'chirpy-20210627.1608';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
